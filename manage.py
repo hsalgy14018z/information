2f11e6995324f649eefbbc5ca613e8639fccd314
hsalgy14018z <zzm2008062499@163.com>
2021-05-26 18:48:03 +0800

    集成Flask_Script

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
 
 
 class Config(object):
@@ -45,6 +46,8 @@ CSRFProtect(app)
 # 设置session保存指定位置
 Session(app)
 
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
@@ -53,4 +56,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
